3379cfe1f5024d3f41cc46072819aefd201d7d44
HaoHaoLifeHack <kk1683788@gmail.com>
2023-10-30 00:17:08 +0800

    test

--- test/USDCV2Test.t.sol
+++ test/USDCV2Test.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.17;
 import "forge-std/Test.sol"; 
 import { RugUsdc } from "../src/USDCV2.sol";
 import { UpgradeableProxy } from "../src/UpgradeableProxy.sol";
+import { Ownable } from "../src/Ownable.sol";
 
 
 contract RugUsdcTest is Test {
@@ -13,17 +14,19 @@ contract RugUsdcTest is Test {
     uint256 BLOCK_NUMBER = vm.envUint("BLOCK_NUMBER");
     string MAINNET_RPC_URL = vm.envString("ETHEREUM_MAINNET_RPC_URL");
 
-    address constant usdcProxyContractAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address payable constant usdcProxyContractAddress = payable(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     address constant usdcOwner = 0xFcb19e6a322b27c06842A71e8c725399f049AE3a;
     address constant usdcAdmin = 0x807a96288A1A408dBC13DE2b1d087d10356395d2;//0x807a96288A1A408dBC13DE2b1d087d10356395d2
     address alice;
     address bob;
+
 
     
     function setUp() public {
         // vm.selectFork(ethMainnetFork);
         // vm.rollFork(BLOCK_NUMBER);
         ethMainnetFork = vm.createSelectFork(MAINNET_RPC_URL, BLOCK_NUMBER);
+        ownable = new Ownable();
         alice = makeAddr("Alice");
         bob = makeAddr("Bob");
     }
@@ -31,8 +34,8 @@ contract RugUsdcTest is Test {
     function testUpgrade() public {
         //upgrade usdc proxy contract && set owner of usdc proxy contract
         vm.startPrank(usdcAdmin);
-        (bool success, ) = UpgradeableProxy(usdcProxyContractAddress).upgradeToAndCall(address(rugUsdc), abi.encodeWithSelector(rugUsdc.initialize.selector, address(alice)));
-        assertEq(success, true);
+        UpgradeableProxy(usdcProxyContractAddress).upgradeToAndCall(address(rugUsdc), abi.encodeWithSelector(rugUsdc.initialize.selector, alice));
+        assertEq(rugUsdc.getOwner(), alice);
         vm.stopPrank();
     }
 
